25c50ca32091e0516488be15274d48aeda963307
beezmo <37284613+beezmo@users.noreply.github.com>
2019-06-10 14:11:23 -0700

    ADD: added output to .txt and rounded value for avg profit change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
-import os
 import csv
 
+#initialize variables
 totalMonths = 0
 netTotal = 0
 maxProfit = 0.0
@@ -16,25 +16,34 @@ with open("budget_data.csv", newline="") as csvfile:
     csvheader = next(csvreader)
 
     for rows in csvreader:
+
+        #Gets first profit entry
         if lineCount:
             lastMonth = int(rows[1])
             lineCount = False
 
+        #Tracks number of rows (=months)
         totalMonths += 1
+        #Calculates total
         netTotal += int(rows[1])
+
+        #Calculates change in profit, appends to list, then sets up for next line
         profitChange = int(rows[1]) - lastMonth
         profitChangeList.append(profitChange)
         lastMonth = int(rows[1])
 
+        #Determines if current profit change is greatest value thus far
         if profitChange > maxProfit:
             maxProfit = profitChange
             maxProfitDate = rows[0]
         
+        #Determines if current profit change is least thus far
         if profitChange < maxLoss:
             maxLoss = profitChange
             maxLossDate = rows[0]
 
-avgChange = sum(profitChangeList) / (len(profitChangeList) - 1)
+#Total profit change divided by (length - 1) to account for initial 0
+avgChange = round(sum(profitChangeList) / (len(profitChangeList) - 1), 2)
 
 print("Financial Analysis")
 print("---------------------------")
@@ -43,3 +52,13 @@ print(f"Total Amount: {netTotal}")
 print(f"Average Change: {avgChange}")
 print(f"Greatest Increase in Profits: {maxProfitDate} (${maxProfit})")
 print(f"Greatest Decrease in Profits: {maxLossDate} (${maxLoss})")
+
+f = open("PyBankAnalysis.txt", "w+")
+print("Financial Analysis", file=f)
+print("---------------------------", file=f)
+print(f"Total Months: {totalMonths}", file=f)
+print(f"Total Amount: {netTotal}", file=f)
+print(f"Average Change: {avgChange}", file=f)
+print(f"Greatest Increase in Profits: {maxProfitDate} (${maxProfit})", file=f)
+print(f"Greatest Decrease in Profits: {maxLossDate} (${maxLoss})", file=f)
+f.close()
